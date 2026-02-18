d206abd323a4b8b5db52ebb80ec1417b8d4fceea
PratikGarai <pratikgarai0208@gmail.com>
2026-02-18 23:19:04 +0530

    dataset input taker

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,13 @@
 import os
+from uuid import uuid4
 
 import cv2
 from numpy import ndarray
 
-from uuid import uuid4
-
-from src.commons.hand_landmarker import HandLandmarkerResult, extract_hand_image_slice, extract_flattened_coordinates
+from constants import FRAME_DELAY_MS
+from src.commons.hand_landmarker import (HandLandmarkerResult,
+                                         extract_flattened_coordinates,
+                                         extract_hand_image_slice)
 
 
 class HandClassificationRawDataset:
@@ -24,11 +26,30 @@ class HandClassificationRawDataset:
         os.makedirs(self.dataset_path, exist_ok=True)
         os.makedirs(self.images_folder, exist_ok=True)
 
-    def add_datapoint(self, image : ndarray, hand_landmarker_result, classes : list[str] = []):
+    def _get_class_selection(self, classes: list[str]) -> str:
+        assert classes is not None and len(
+            classes) != 0, "Classes list cannot be empty or None"
+
+        options_str = ""
+        for i, c in enumerate(classes):
+            options_str += f"[{i}] {c}\n"
+        print(options_str)
+        print("Select data class : ")
+        key = cv2.waitKey(FRAME_DELAY_MS) & 0xFF
+        try:
+            key = key - ord('0')
+            choice = int(key)
+            assert choice > 0 and choice < len(classes+1)
+            return classes[choice-1]
+        except:
+            print("Invalid option selected.")
+
+    def add_datapoint(self, image: ndarray, hand_landmarker_result, classes: list[str] = []):
         assert image is not None, "Image cannot be null"
         assert image is not ndarray, "Image has to be an ndarray"
         assert hand_landmarker_result is not None, "Hand landmarker result cannot be null"
-        assert classes is not None and len(classes) != 0, "Classes list cannot be empty or None"
+        assert classes is not None and len(
+            classes) != 0, "Classes list cannot be empty or None"
 
         try:
             validated_landmarker_result = HandLandmarkerResult(
@@ -40,6 +61,10 @@ class HandClassificationRawDataset:
         landmarks = validated_landmarker_result.landmarks
         world_landmarks = validated_landmarker_result.world_landmarks
 
+        element_id = str(uuid4())
+        print(
+            "\n\n============================\nGenerating datapoint for id : ", element_id)
+
         # Image Extraction
         subimage = extract_hand_image_slice(image, landmarks)
         cv2.imshow("Captured subimage", subimage)
@@ -47,5 +72,5 @@ class HandClassificationRawDataset:
         # Flattened Coordinates Extraction
         coords = extract_flattened_coordinates(world_landmarks)
 
-        element_id = str(uuid4())
-        print("Generating")
+        # option = self._get_class_selection(classes)
+        # print(option)
